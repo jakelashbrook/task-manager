802ea71321bc83b9e55b57b963660346a941ea87
jakelashbrook <jakeashleylashbrook@gmail.com>
2021-05-01 09:28:39 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+        format: "dd mmmm yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: { 
+            done: "Select"
+            // i18n is the abbreviation for internationalization and localization 
+        }
+    });
   });
 
 
